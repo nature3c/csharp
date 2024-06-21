b79fafb40a25e31e84f37ff4da3dee00ec3b6d84
Andrew Bae <61103759+nature3c@users.noreply.github.com>
2024-06-21 18:51:00 +0900

    day 4.1

---
+++ PJT_DICE/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT_DICE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dice1, dice2, dice3, dice4, dice5, dice6;
+            int throwCount = 0, serialCount = 0;
+
+            Random random = new Random();
+
+            while(true)
+            {
+                throwCount++;
+                dice1 = random.Next(0, 7);
+                dice2 = random.Next(0, 7);
+                dice3 = random.Next(0, 7);
+                dice4 = random.Next(0, 7);
+                dice5 = random.Next(0, 7);
+                dice6 = random.Next(0, 7);
+
+                if (dice1 == dice2 && dice2 == dice3 && dice3 == dice4 && dice4 == dice5 && dice5 == dice6)
+                {
+                    Console.WriteLine("All the dice are the same: " + dice1);
+                    break;
+                }
+                else if ((dice1 == 1 || dice2 == 1 || dice3 == 1 || dice4 == 1 || dice5 == 1 || dice6 == 1) &&
+                        (dice1 == 2 || dice2 == 2 || dice3 == 2 || dice4 == 2 || dice5 == 2 || dice6 == 2) &&
+                        (dice1 == 3 || dice2 == 3 || dice3 == 3 || dice4 == 3 || dice5 == 3 || dice6 == 3) &&
+                        (dice1 == 4 || dice2 == 4 || dice3 == 4 || dice4 == 4 || dice5 == 4 || dice6 == 4) &&
+                        (dice1 == 5 || dice2 == 5 || dice3 == 5 || dice4 == 5 || dice5 == 5 || dice6 == 5) &&
+                        (dice1 == 6 || dice2 == 6 || dice3 == 6 || dice4 == 6 || dice5 == 6 || dice6 == 6)) ;
+
+                serialCount++;
+            }
+            Console.WriteLine("Number of rolls: " + throwCount);
+            Console.WriteLine("Number of consecutive number rolls: " + serialCount +"\n");
+        }
+    }
+}
---
+++ day4project/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //proj5_2
+            //int year;
+            //Console.Write("Input the year: ");
+            //year = int.Parse(Console.ReadLine());
+
+            //if(((year % 4 == 0) && ( year %100 != 0)) || (year % 400 == 0))
+            //{
+            //    Console.WriteLine("{0:D} is a leap year", year);
+            //} else
+            //{
+            //    Console.WriteLine("{0:D} is not a leap year", year);
+            //}
+
+            //proj5_3
+            //int a = 200;
+
+            //if(a < 100)
+            //{
+            //    Console.WriteLine("Less than 100");
+            //    Console.WriteLine("If true youll see this");
+            //}
+
+            //Console.WriteLine("End of program");
+
+            //proj5_5
+            //int a = 200;
+
+            //if (a < 100)
+            //{
+            //    Console.WriteLine("Less than 100");
+            //    Console.WriteLine("If true youll see this");
+            //} else
+            //{
+            //    Console.WriteLine("Larger than 100");
+            //    Console.WriteLine("If false then youll see this");
+            //}
+
+            //Console.WriteLine("End of program");
+
+            //proj5_6
+            //int a;
+            //Console.Write("Input a number: ");
+            //a = int.Parse(Console.ReadLine());
+
+            //if((a%3==0)||(a%5==0))
+            //{
+            //    Console.WriteLine("Multiple of 3 and 5");
+            //} else
+            //{
+            //    Console.WriteLine("Not a multiple of 3 or 5");
+            //}
+
+            //proj5_8
+            //int score;
+            //Random random = new Random();
+
+            //for(int i= 0; i < 5; i++)
+            //{
+            //    score = random.Next(0, 100);
+            //    Console.Write("Total Score " + score + " ==> ");
+
+            //    if (score >= 90)
+            //        Console.WriteLine("A");
+            //    else
+            //        if (score >= 80)
+            //        Console.WriteLine("B");
+            //        else
+            //            if(score >= 70)
+            //            Console.WriteLine("C");
+            //                else
+            //                    if(score >= 60)
+            //                    Console.WriteLine("D"); 
+            //                        else
+            //                            Console.WriteLine("F"); 
+
+            //}
+            //Console.WriteLine("Grades");
+
+            //proj5_10
+            //int a;
+
+            //Console.Write("Choose a number 1-4: ");
+            //a = int.Parse(Console.ReadLine());
+
+            //switch (a)
+            //{
+            //    case 1:
+            //        Console.WriteLine("1 selected\n");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("2 selected\n");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("3 selected\n");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("4 selected\n");
+            //        break;
+            //    default:
+            //        Console.WriteLine("That was not an option\n");
+            //        break;
+            //}
+
+            int score;
+
+            Random random = new Random();
+
+            for(int i =0; i < 5; i++)
+            {
+                score = random.Next(0, 100);
+                Console.Write("Total Score " + score + " ==> ");
+
+                switch(score/10)
+                {
+                    case 10:
+                    case 9:
+                        Console.Write('A');
+                        break;
+                    case 8:
+                        Console.Write('B');
+                        break;
+                    case 7:
+                        Console.Write('C');
+                        break;
+                    case 6:
+                        Console.Write('D');
+                        break;
+                    default:
+                        Console.Write('F');
+                        break;
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
